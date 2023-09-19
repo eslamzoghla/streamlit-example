af03203bad627f32f2e6e7211835d420c4866ea5
Eslam Zoghla <95759229+eslamzoghla@users.noreply.github.com>
2023-09-20 02:12:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,29 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import openai  # Assuming you're using OpenAI's GPT API
 
-"""
-# Welcome to Streamlit!
+# Configure OpenAI API
+openai.api_key = "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"  # Replace with your OpenAI API key
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# GPT-3.5 model prompt
+gpt3_prompt = "You are a helpful assistant."
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Function to generate response using GPT-3.5
+def generate_response(user_input):
+    prompt = f"{gpt3_prompt}\nUser: {user_input}\nAssistant:"
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=50
+    )
+    return response.choices[0].text.strip()
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Streamlit UI
+st.title("Chat with GPT-3.5")
+st.write("You are chatting with a GPT-3.5 powered assistant.")
 
+user_input = st.text_input("You:", "")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if user_input:
+    # Generate response using GPT-3.5
+    response = generate_response(user_input)
+    st.text_area("Assistant:", response, height=100)
